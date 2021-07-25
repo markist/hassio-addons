894eff4aa3e0dabf0de527488f3efc6827d133b4
markist <martin.kistler@gmail.com>
2021-07-25 14:03:36 +0200

    run squeezelite.sh

---
+++ squeezelite/run-squeezelite.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "About to sleep for $STARTUP_DELAY_SEC second(s)"
+sleep $STARTUP_DELAY_SEC
+echo "Rise and shine!"
+
+if [ "$SQUEEZELITE_SPECIFY_SERVER" == "yes" ]; then \
+  echo "Server has been specified: $SQUEEZELITE_SPECIFY_SERVER"; \
+  /usr/bin/squeezelite \
+    -s $SQUEEZELITE_SERVER_PORT \
+    -n $SQUEEZELITE_NAME \
+    -o $SQUEEZELITE_AUDIO_DEVICE \
+    -C $SQUEEZELITE_TIMEOUT \
+    -D $SQUEEZELITE_DELAY; \
+else \
+  echo "Server has not been specified, using discovery"; \
+  /usr/bin/squeezelite \
+    -n $SQUEEZELITE_NAME \
+    -o $SQUEEZELITE_AUDIO_DEVICE \
+    -C $SQUEEZELITE_TIMEOUT \
+    -D $SQUEEZELITE_DELAY; \
+fi
